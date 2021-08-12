11077d088265f41924c6a874f0aa063cee59d4ec
Igor Nunes <igornj@hotmail.com>
2021-08-12 14:52:35 -0300

    dale

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/', upload.single('file'), async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3008;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
