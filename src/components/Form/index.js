1997bdefff5eba85ca8c56a55c55c261f3555725
Igor Nunes <igornj@hotmail.com>
2021-08-26 18:45:05 -0300

    dale

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,28 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from '../../styles/GlobalStyles';
-import { Button, Title } from './styled';
+import { Button, Title, Paragraph, Container } from './styled';
 
 export default function Form({ handleChange, handleSend }) {
   return (
     <>
       <Title>Upper Moments</Title>
+      <Paragraph>Envie seus rolês abaixo</Paragraph>
+
       <Container>
         <form action="#" method="POST" encType="multipart/form-data">
-          <input type="file" name="image" onChange={handleChange} />
+          <input type="file" name="image" id="file" onChange={handleChange} />
+
+          <h4>Regras</h4>
+          <ul>
+            <li>
+              1 - Não seja babaquinha, envie somente fotos de rolês do clan 👌
+            </li>
+            <li>2 - Sua mãe é minha 👵</li>
+          </ul>
+
           <Button
             type="submit"
             name="submit"
@@ -22,6 +33,10 @@ export default function Form({ handleChange, handleSend }) {
           </Button>
         </form>
       </Container>
+      <Paragraph>
+        Sim ta uma merda ainda, porém está funcionando e em breves ficará mais
+        meió. Agradeço a compreensão e VAMU CLANN!
+      </Paragraph>
     </>
   );
 }
--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -1,14 +1,59 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
+  display: block;
   padding: 10px 20px;
-  margin-top: 1rem;
+  margin: 1.5rem auto;
   background-color: red;
   color: white;
+  border-radius: 4px;
 `;
 
 export const Title = styled.h1`
   text-align: center;
   margin-top: 1rem;
   color: red;
+`;
+
+export const Paragraph = styled.p`
+  text-align: center;
+  margin-top: 1rem;
+  font-weight: bold;
+`;
+
+export const Container = styled.section`
+  max-width: 360px;
+  background: #fff;
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  /*   input {
+    display: none;
+  }
+
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #eee;
+    border: 5px solid red;
+    margin: 5px auto;
+    cursor: pointer;
+    border-radius: 50%;
+    overflow: hidden;
+  } */
+
+  h4 {
+    margin: 2.2rem 1rem 1rem 0;
+  }
+
+  li {
+    list-style: none;
+    margin: 1rem 1rem 1rem 0;
+    font-size: 15px;
+  }
 `;
