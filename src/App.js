4feb8ed49bad353d90c1b510e24ed85de947e861
Igor Nunes <igornj@hotmail.com>
2021-08-26 17:20:31 -0300

    dale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import GlobalStyle from './styles/GlobalStyles';
 
 import Routes from './routes';
