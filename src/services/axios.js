94451ba115dfa563ca6647d9d51d0c195f9a0fa8
Igor Nunes <igornj@hotmail.com>
2021-11-11 15:46:57 -0300

    Mudando link do axios pra url do novo projeto dentro do heroku

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://discord-bot-upload.herokuapp.com/',
+  // baseURL: 'https://discord-bot-upload.herokuapp.com/',
+  baseURL: 'https://discord-bot-upload-2.herokuapp.com',
 });
