b26153fd1a364cb53a3c9b3792088b64de450475
Igor Nunes <igornj@hotmail.com>
2021-11-15 14:06:43 -0300

    dale

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://discord-bot-upload-2.herokuapp.com/',
+  baseURL: 'https://discord-bot-upload-2.herokuapp.com',
   // baseURL: 'https://discord-bot-authentication.firebaseapp.com/',
   // baseURL: 'http://localhost:3001/',
 });
