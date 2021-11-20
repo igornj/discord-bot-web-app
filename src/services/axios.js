3033c162404044624a3f389f9f1e0530737ef253
Igor Nunes <igornj@hotmail.com>
2021-11-20 11:10:59 -0300

    tentativa heroku

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://discord-bot-web-app.vercel.app/',
+  // baseURL: 'https://discord-bot-web-app.vercel.app/',
   // baseURL: 'https://discord-bot-authentication.firebaseapp.com/',
   // baseURL: 'http://localhost:3001/',
+  basseURL: 'https://discord-bot-web-app.herokuapp.com/',
 });
