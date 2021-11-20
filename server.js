c0a05ff1e276db7c48320d23332559252656ff0d
Igor Nunes <igornj@hotmail.com>
2021-11-20 11:33:23 -0300

    static files in express

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
