ab91b18fe933fff4449ce8a101af467938b82726
Igor Nunes <igornj@hotmail.com>
2021-11-19 22:01:37 -0300

    daleoa kdopa

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -40,7 +40,7 @@ export default function HomePage() {
       <Container>
         <Button>
           <Link
-            to="/upload"
+            to="/api/upload"
             style={{
               textDecoration: 'none',
               color: 'white',
