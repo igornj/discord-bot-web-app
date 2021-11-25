46dcf7ab0dfbdedd71930f7585bf6d5f0bca3704
Igor Nunes <igornj@hotmail.com>
2021-11-25 12:42:58 -0300

    daijsdo

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://discord-bot-web-app.vercel.app/',
+  baseURL: 'https://discord-bot-web-app.vercel.app/',
   // baseURL: 'https://discord-bot-authentication.firebaseapp.com/',
   // baseURL: 'http://localhost:3001/',
-  baseURL: 'https://discord-bot-web-app-eiil4wf4s-igornj.vercel.app',
+  // baseURL: 'https://discord-bot-web-app.herokuapp.com/',
 });
