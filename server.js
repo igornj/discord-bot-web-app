c555ffd32c9af77e4cde062a70d146c5e7a6d321
Igor Nunes <igornj@hotmail.com>
2021-11-26 12:05:37 -0300

    cors

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true, // access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 app.use(express.static(path.join(__dirname, 'build')));
