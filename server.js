658c3e641c3e0901f0f429eb66ddb3e6a42f158c
Igor Nunes <igornj@hotmail.com>
2021-11-26 13:47:17 -0300

    cors

--- server.js
+++ server.js
@@ -12,29 +12,7 @@ const routes = require('./routes');
 
 const app = express();
 
-// const corsOptions = {
-//   origin: 'https://discord-bot-web-app.herokuapp.com',
-//   credentials: true, // access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-
-  // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(express.static(path.join(__dirname, 'build')));
